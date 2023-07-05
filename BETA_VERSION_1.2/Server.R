007be808fda5b6bb68089752d81d264fec365219
abbyejohnson <104595929+abbyejohnson@users.noreply.github.com>
2023-07-05 14:02:09 -0500

    Delete BETA_VERSION_1.2 directory

--- BETA_VERSION_1.2/Server.R
+++
@@ -1,6 +0,0 @@
-SERVER <- function(input, output, session) {
-  
-  source("SERVER_Field_Management.R", local = TRUE)
- # source("SERVER_Rotation_Management.R", local = TRUE)
-  source("SERVER_About.R", local = TRUE)
-}
